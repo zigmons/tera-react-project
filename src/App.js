d048f602c3d51b63a2cc6e86d7c319e474acfe6e
zigmons <rafael_sousa87@hotmail.com>
2023-02-16 21:57:16 -0300

    Adicionador roteamento

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-// import Home from "./components/pages/Home";
-// import UserBlog from "./components/pages/UserBlog";
-// import User from "./components/pages/User";
+import Home from "./components/pages/Home";
+import UserBlog from "./components/pages/UserBlog";
+import User from "./components/pages/User";
 import CreatePost from "./components/pages/CreatePost";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 import "./styles/normalize.css"
 import "./styles/fontawesome.min.css"
@@ -10,10 +11,15 @@ import "./styles/main.css"
 
 function App() {
   return (
-    // <Home/>
-    // <UserBlog/>
-    // <User/>
-    <CreatePost/>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/users" element={<User/>} />
+      <Route path="/users/:userId" element={<UserBlog/>} />
+      <Route path="/users/:userId/posts/new" element={<CreatePost/>} />
+      <Route path="*" element={<div>Not Found</div>} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
