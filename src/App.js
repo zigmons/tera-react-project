21c0e96f944fc69e042761f1d9be41f7ea50bf68
zigmons <rafael_sousa87@hotmail.com>
2023-02-16 21:37:05 -0300

    p2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Home from "./components/pages/Home";
+// import Home from "./components/pages/Home";
+// import UserBlog from "./components/pages/UserBlog";
+// import User from "./components/pages/User";
+import CreatePost from "./components/pages/CreatePost";
+
 import "./styles/normalize.css"
 import "./styles/fontawesome.min.css"
 import "./styles/main.css"
 
+
 function App() {
   return (
-    <Home/>
+    // <Home/>
+    // <UserBlog/>
+    // <User/>
+    <CreatePost/>
   );
 }
 
