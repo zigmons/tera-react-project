7a64dafc8444d65e98a3fffb35186d16bfb0d523
zigmons <rafael_sousa87@hotmail.com>
2023-02-16 21:09:36 -0300

    projeto em andamento

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Home from "./components/pages/Home";
+import "./styles/normalize.css"
+import "./styles/fontawesome.min.css"
+import "./styles/main.css"
+
 function App() {
   return (
-    <div>Projeto</div>
+    <Home/>
   );
 }
 
 export default App;
+
