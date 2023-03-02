3179b401872dc6892f59e8e5eab5978420540483
zigmons <rafael_sousa87@hotmail.com>
2023-03-02 20:15:35 -0300

    Adicionado API em form

--- src/components/pages/UserPostForm.jsx
+++ src/components/pages/UserPostForm.jsx
@@ -1,21 +1,55 @@
 import React from "react";
 
 import Default from "../templates/Default";
+import AppLoading from "../organisms/AppLoading";
 
 export default function UserPostForm() {
-  return (
+  const [title, setTitle] = React.useState("");
+  const [content, setContent] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    fetch("https://63cf09718a780ae6e6710dbe.mockapi.io/users/1/posts", {
+      method: "POST",
+      body: JSON.stringify({ title, content }),
+      headers: { "Content-type": "application/json" },
+    }).then(() => {
+      setTitle("");
+      setContent("");
+    });
+    setIsLoading(false);
+  };
+
+  return isLoading ? (
+    <AppLoading />
+  ) : (
     <Default>
       <div className="create-post">
         <h1>Criar</h1>
 
-        <form className="create-post__form">
+        <form onSubmit={handleFormSubmit} className="create-post__form">
           <div className="create-post__form-name">
             <label htmlFor="name">Título</label>
-            <input type="text" id="name" name="title" />
+            <input
+              type="text"
+              id="name"
+              name="title"
+              value={title}
+              required
+              onChange={(event) => setTitle(event.target.value)}
+            />
           </div>
           <div className="create-post__form-content">
             <label htmlFor="content">Conteúdo</label>
-            <textarea name="content" id="content"></textarea>
+            <textarea
+              name="content"
+              id="content"
+              value={content}
+              onChange={(event) => setContent(event.target.value)}
+            ></textarea>
           </div>
           <button className="button-primary">Salvar</button>
         </form>
